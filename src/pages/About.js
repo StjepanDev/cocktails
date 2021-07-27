b6723a372532f9293e595f1562ea2f520ecf2a79
StjepanDev <grunf69@yahoo.com>
2021-07-27 14:28:42 +0200

    api call

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur,
+        facilis. Laudantium quibusdam sed distinctio, error quam rem quidem,
+        voluptate vel ad quia doloremque! Eius, quasi! Nihil recusandae
+        voluptate a aperiam!
+      </p>
+    </section>
+  );
+};
+
+export default About;
